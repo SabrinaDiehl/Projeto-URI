1c6912894c52c0425bcb8505aa1ab012a0d11c93
SabrinaDiehl <32180670+SabrinaDiehl@users.noreply.github.com>
2017-12-04 13:44:32 -0200

    Commit com as alterações finais

---
+++ Uri-master/test/Uri1142/PumTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uri1142;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sabrina.diehl
+ */
+public class PumTest {
+    
+    public PumTest() {
+    }
+
+    /**
+     * Test of gerarSequencia method, of class Pum.
+     */
+    @Test
+    public void testGerarSequencia() {
+        Pum pum = new Pum();
+        String experado = "1 2 3 PUM\n";
+        String result = pum.gerarSequencia(1);
+        assertEquals(experado, result);
+    }
+    
+}
---
+++ Uri-master/test/Uri1151/FibonacciTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Uri1151;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sabrina.diehl
+ */
+public class FibonacciTest {
+    
+    public FibonacciTest() {
+    }
+
+    /**
+     * Test of gerarFibonacci method, of class Fibonacci.
+     */
+    @Test
+    public void testGerarFibonacci() {
+        Fibonacci fibonacci = new Fibonacci();
+        String experado = "55 ";
+        String result = fibonacci.gerarFibonacci(11);
+        assertEquals(experado, result);
+        
+    }
+    
+}
